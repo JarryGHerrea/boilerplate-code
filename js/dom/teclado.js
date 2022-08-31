3fa7293c6875670869b18cf45705127e90ad6e1a
JarryGHerrera <jarryheremyherrera@gmail.com>
2022-08-31 06:37:37 -0700

    Event keyboard (Move ball).

---
+++ js/dom/teclado.js
@@ -0,0 +1,46 @@
+const d = document;
+let x = 0,
+  y = 0;
+export function moverBall(e, ball, stage) {
+  const $ball = d.querySelector(ball),
+    $stage = d.querySelector(stage),
+    limitBall = $ball.getBoundingClientRect(),
+    limitStage = $stage.getBoundingClientRect();
+  console.log(e.keyCode);
+
+  //   const move = (direcion) => {};
+
+  switch (e.keyCode) {
+    case 37:
+      if (limitsBall.left > limitsStage.left) {
+        e.preventDefault();
+        x--;
+      }
+
+      break;
+    case 38:
+      if (limitsBall.top < limitsStage.top) {
+        e.preventDefault();
+        y--;
+      }
+
+      break;
+    case 39:
+      if (limitsBall.right < limitsStage.right) {
+        e.preventDefault();
+        x++;
+      }
+      break;
+    case 40:
+      if (limitsBall.bottom < limitsStage.bottom) {
+        e.preventDefault();
+        y++;
+      }
+      break;
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x * 10}px${y * 10}px)`;
+}
+
+export function shortcuts(e) {}
